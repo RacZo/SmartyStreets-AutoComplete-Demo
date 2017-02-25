b4a863c3d3c5e1bc4df85e3fe7502d1c3e815a4f
Oscar S <oscar@realactionsoft.com>
2017-02-25 15:32:23 -0500

    Updated gradle library dependencies Updated Navigation Drawer header avatar image Removed unused menu and string resources Bumped up version code to 4 and version name to 1.3

--- app/src/main/java/com/oscarsalguero/smartystreetsautocomplete/demo/MainActivity.java
+++ app/src/main/java/com/oscarsalguero/smartystreetsautocomplete/demo/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -102,26 +101,10 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Pass the event to ActionBarDrawerToggle, if it returns
         // true, then it has handled the app icon touch event
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
             return true;
         }
 
